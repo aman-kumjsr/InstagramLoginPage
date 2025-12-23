7f24917a7ead150d2082a3b9f80b9c7d7b612e15
Aman Kumar <amankumar@Amans-MacBook-Air.local>
2025-12-24 02:18:21 +0530

    changed server.js to store username and password

--- server.js
+++ server.js
@@ -20,44 +20,23 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.log(err));
 
-/* REGISTER */
-app.post("/register", async (req, res) => {
+app.post("/login", async (req, res) => {
+  try {
     const { username, password } = req.body;
 
-    // const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+      username,
+      password   // plain text (learning only)
+    });
 
-    try {
-        const user = new User({
-            username,
-            password: password  // plain - text password
-        });
-        await user.save();
-        res.json({ message: "User registered successfully" });
-    } catch {
-        res.status(400).json({ error: "User already exists" });
-    }
+    await user.save();
+
+    res.json({ message: "Data saved successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to save data" });
+  }
 });
 
-/* LOGIN */
-app.post("/login", async (req, res) => {
-    try {
-        const { username, password } = req.body;
-
-        const user = await User.findOne({ username });
-        if (!user) return res.status(400).json({ error: "Invalid credentials" });
-
-        //   const isMatch = await bcrypt.compare(password, user.password);
-        //   if (!isMatch) return res.status(400).json({ error: "Invalid credentials" });
-
-        if (user.password !== password) {
-            return res.status(400).json({ error: "Invalid credentials" });
-        }
-
-        res.json({ message: "Login successful" });
-    } catch (err) {
-        res.status(500).json({ error: "Server error" });
-    }
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
