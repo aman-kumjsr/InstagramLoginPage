a3135a69620cffaac3c713e025b8eadefe49e986
Aman Kumar <amankumar@Amans-MacBook-Air.local>
2026-03-11 21:48:16 +0530

    Modified to deploy in Vercel

--- api/server.js
+++ api/server.js
@@ -1,33 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
-//const bcrypt = require("bcryptjs");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const User = require("./models/User");
+const User = require("../models/User");
 require("dotenv").config();
 
 const app = express();
 
 const path = require("path");
-app.use(express.static(path.join(__dirname, "public")));
-
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.use(cors());
 app.use(bodyParser.json());
 
-// Connect MongoDB
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB Connected"))
-    .catch(err => console.log(err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 app.post("/login", async (req, res) => {
   try {
     const { username, password } = req.body;
 
-    const user = new User({
-      username,
-      password   // plain text (learning only)
-    });
+    const user = new User({ username, password });
 
     await user.save();
 
@@ -37,7 +31,4 @@ app.post("/login", async (req, res) => {
   }
 });
 
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
+module.exports = app;
